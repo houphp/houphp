b960b96c7d767c2fa90bd188927a07d20880c8ef
houphp <2869571987@qq.com>
2019-08-24 18:39:56 +0800

    base

--- index/public/index.php
+++ index/public/index.php
@@ -1,4 +1,4 @@
 <?php
 $rootPath = dirname(__DIR__);
-require '/www/wwwroot/pro/houphp/vendor/houjit/houphp-framework' . DIRECTORY_SEPARATOR . 'houphp.php';
-houphp::run($rootPath);
+
+houphp::run(\houphp\houphp);
