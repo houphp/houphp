7024123dc110f79f4839b1e128555f00d091da51
houphp <2869571987@qq.com>
2019-08-24 23:43:01 +0800

    base

---
+++ home/webroot/main.php
@@ -0,0 +1,5 @@
+<?php
+use ZPHP\ZPHP;
+$rootPath = dirname(__DIR__);
+require '/Users/amosliu/www/houphp/vendor/zphp/zphp'. DIRECTORY_SEPARATOR . 'ZPHP' . DIRECTORY_SEPARATOR . 'ZPHP.php';
+ZPHP::run($rootPath);
