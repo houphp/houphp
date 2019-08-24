06899977e63266e9c853cc44457922c79ded518e
houphp <2869571987@qq.com>
2019-08-24 18:05:04 +0800

    base

--- index/public/index.php
+++ index/public/index.php
@@ -1,5 +1,5 @@
 <?php
 $rootPath = dirname(__DIR__);
-var_dump($rootPath);exit;
+var_dump($rootPath.'/../vendor/houjit/houphp-framework');exit;
 require '/Users/amosliu/www/houphp/vendor/houjit/houphp-framework' . DIRECTORY_SEPARATOR . 'houphp.php';
 houphp::run($rootPath);
