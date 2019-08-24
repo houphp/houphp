a042a157f316b79a39da490a1fbeaf37c3e3d29e
houphp <2869571987@qq.com>
2019-08-24 17:22:53 +0800

    base

--- index/public/index.php
+++ index/public/index.php
@@ -1,7 +1,6 @@
 <?php
-use houphp;
+use index;
 $rootPath = dirname(__DIR__);
-
 require '/Users/amosliu/www/houphp/vendor/houjit/houphp-framework' . DIRECTORY_SEPARATOR . 'houphp.php';
-
+var_dump($rootPath);exit;
 houphp::run($rootPath);
