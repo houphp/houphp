621d3a204dcd27eb8192105efef8eb43e0f8eb4d
houphp <2869571987@qq.com>
2019-08-24 18:14:31 +0800

    base

--- index/public/index.php
+++ index/public/index.php
@@ -1,5 +1,5 @@
 <?php
 $rootPath = dirname(__DIR__);
-require $rootPath . '/../vendor/houjit/houphp-framework' . DIRECTORY_SEPARATOR . 'houphp.php';
-var_dump($rootPath . '/../vendor/houjit/houphp-framework' . DIRECTORY_SEPARATOR . 'houphp.php');exit;
+require $rootPath . '/../../vendor/houjit/houphp-framework' . DIRECTORY_SEPARATOR . 'houphp.php';
+var_dump($rootPath . '/../../vendor/houjit/houphp-framework' . DIRECTORY_SEPARATOR . 'houphp.php');exit;
 houphp::run($rootPath);
